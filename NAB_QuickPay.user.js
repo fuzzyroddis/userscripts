84cbf7124c8c0bf1be2f6aa0eb53823942ed4978
stevenroddis <stevenroddis@users.noreply.github.com>
2014-03-09 17:11:09 +1100

    BPAY Support! And Direct Deposit now supports name field.

--- NAB_QuickPay.user.js
+++ NAB_QuickPay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       NAB QuickPay
 // @namespace  https://nabquickpay.stevenroddis.com
-// @version    0.1
+// @version    0.2
 // @description  A work in progress! Creates a new way to pay, just copy and paste payment information into the textarea and it'll auto fill account/bpay info and amount.
 // @match      https://ib.nab.com.au/*
 // @author     Steven Roddis
@@ -16,9 +16,10 @@ var BPAY_TYPE = 1;
 function parsePaymentString(str) {
     str_stripped = str.toLowerCase().replace(/[ -]/g, "");
     
-	//Direct Deposit    
+    //Direct Deposit    
     var bsbRegex = /(?:^|[^$])([0-9]{6})(?:[^0-9]|$)/;
     var accountRegex = /(?:^|[^$])([0-9]{8,9})(?:[^0-9]|$)/; //I've seen plenty of 8 digit account numbers.
+    var nameRegex = /\bname\:\s*(.+)/i;
     
     //BPAY
     var billerCodeRegex = /(?:bpay|biller|code).+?[^$]([0-9]{4,6})/i;
@@ -49,6 +50,11 @@ function parsePaymentString(str) {
     if (amount != null) {
         amount = amount[1];
     }
+    
+    var name = nameRegex.exec(str);
+    if (name != null) {
+        name = name[1];
+    }
       
     var billerCode = billerCodeRegex.exec(str_stripped);
     var referenceNum = null;
@@ -68,36 +74,80 @@ function parsePaymentString(str) {
     switch(paymentType)
     {
         case DEPOSIT_TYPE:
-        	return [paymentType, amount, bsb, account];
+            return [paymentType, amount, bsb, account, name];
         break;
                         
         case BPAY_TYPE:
-        	return [paymentType, amount, billerCode, referenceNum];
+            return [paymentType, amount, billerCode, referenceNum];
         break;
         
         default:
-    	case UNKNOWN_TYPE:
-        	if(amount)
-            	return [UNKNOWN_TYPE, amount];
-        	else
-        		return [UNKNOWN_TYPE];
+        case UNKNOWN_TYPE:
+            if(amount)
+                return [UNKNOWN_TYPE, amount];
+            else
+                return [UNKNOWN_TYPE];
         break;
     }           
 }
 
-//input
-ele = '<textarea id="stevenroddis-quickpay"></textarea>';
+function fillForms(p) {
+    if(isBPAY)
+    {
+        if($("input[name=billerCode]").length)
+        {
+            //first step
+            $("input:radio[name=searchType]")[1].checked = true;
+            $("input[name=billerCode]").val(p[2]);
+            //Magic Below
+            //I also had the idea of passing via target or window name
+            data = p;
+            for(var i = 0; i < data.length; i++)
+                data[i] = encodeURIComponent(data[i]); //make it ready for implosion
+            data = data.join(',');
+            $("form[name=editBillPaymentForm]").attr("action", $("form[name=editBillPaymentForm]").attr("action") + "#SRQP!"+data);
+        }
+        //second step
+        else
+        {
+            $("input[name=custRefNumber]").val(p[3]);
+            $("input[name=paymentAmount]").val(p[1]);
+        }
+    }
+    else
+    {
+        $("#payeeBsb").val(p[2]);
+        $("#payeeAcctId").val(p[3]);
+        $("#amount").val(p[1]);
+        $("#payeeAcctName").val(p[4]);
+    }
+}
 
-//Add input to document
-$("#payeeAcctId + p").html(ele);
+//input
+var ele = '<textarea id="stevenroddis-quickpay"></textarea>';
+var isBPAY = false;
+
+if(location.pathname.indexOf("billPayment") > -1)
+    isBPAY = true;
+else
+    isBPAY = false;
+
+if(location.hash.substring(0,6) == "#SRQP!") //have we passed details from last page?
+{
+    p = location.hash.substring(6).split(',');
+    for(var i = 0; i < p.length; i++)
+        p[i] = decodeURIComponent(p[i]); //unescape
+    fillForms(p);
+}
+else if(isBPAY) //add form
+    $("form[name=editBillPaymentForm] table.tdSectionTable + table td").first().html(ele);
+else
+    $("#payeeAcctId + p").html(ele);
 
 //Bind on change
 $("#stevenroddis-quickpay").bind('input propertychange', function() {
-p = parsePaymentString(this.value);
-
-$("#payeeBsb").val(p[2]);
-$("#payeeAcctId").val(p[3]);
-$("#amount").val(p[1]);
+    p = parsePaymentString(this.value);
+    fillForms(p); //fill in values
 });
 
 //Style
