ba7399c36d680b595dfe0c43ae9e047aa8da39ee
stevenroddis <stevenroddis@users.noreply.github.com>
2014-03-09 11:55:13 +1100

    Made script to generate a readme.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-userscripts
-===========
+Steven's Userscript Collection
+==============================
+*A collection of userscripts I have created or modified.*
+[NAB QuickPay](https://github.com/StevenRoddis/userscripts/blob/master/NAB_QuickPay.user.js)
+------------
+Author: Steven Roddis
+Description: A work in progress! Creates a new way to pay, just copy and paste payment information into the textarea and it'll auto fill account/bpay info and amount.
 
-A collection of userscripts I have created or modified.
+------------
+[Screw You NAB: I Don't Want a New Window Edition](https://github.com/StevenRoddis/userscripts/blob/master/screwyounab.user.js)
+------------
+Author: Steven Roddis
+
+------------
---
+++ utils/readme.php
@@ -0,0 +1,53 @@
+#!/usr/bin/php
+<?php
+/*
+	Readme Generator for my Userscript Repository
+*/
+error_reporting(E_ALL);
+
+function getDetails($str)
+{
+	$details = array();
+
+	preg_match('#//\s+==UserScript==([\s\S]+?)//\s+==/UserScript==#', $str, $matches);
+	$matches = $matches[1];
+
+	preg_match('#// @name\s+(.+)#', $matches, $temp);
+	$details['name'] = $temp[1];
+
+	preg_match('#// @description\s+(.+)#', $matches, $temp);
+	$details['description'] = $temp[1];
+
+	preg_match('#// @author\s+(.+)#', $matches, $temp);
+	$details['author'] = $temp[1];
+
+	return $details;
+
+}
+
+$readme = <<<EOT
+Steven's Userscript Collection
+==============================
+*A collection of userscripts I have created or modified.*
+EOT;
+
+$scripts = scandir('../', SCANDIR_SORT_ASCENDING);
+
+foreach($scripts as $script)
+{
+	if(preg_match('/\.user\.js$/', $script)) //userscripts only
+	{
+		$file = file_get_contents('../'.$script);
+		$details = getDetails($file);
+
+		$readme .= "\n[".$details['name'].'](https://github.com/StevenRoddis/userscripts/blob/master/'.$script.")\n------------"; //name & subheading
+		if($details['author'])
+			$readme .= "\nAuthor: ".$details['author'];
+		if($details['description'])
+			$readme .= "\nDescription: ".$details['description'];
+		$readme .= "\n\n------------";
+	}	
+}
+
+file_put_contents('../README.md', $readme);
+?>
