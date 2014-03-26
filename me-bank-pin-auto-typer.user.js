243652db25a67c5a4f650fe18c05a73331b3c697
stevenroddis <stevenroddis@users.noreply.github.com>
2014-03-26 13:00:23 +1100

    Committing this as it's finally at a stage other people can use it.

---
+++ me-bank-pin-auto-typer.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name       Me Bank PIN Auto-Typer
+// @namespace  com.stevenroddis.com.mebank.pinautotyper
+// @version    0.2
+// @description  When a password manager autofills the password field this script types it out on the stupid PIN pad.
+// @match      https://ib.mebank.com.au/*
+// ==/UserScript==
+
+if($("#numericScramblePad").length) //are we on the right page?
+{
+    $("#txtPassword").change(function () {
+        access_code = $("#txtPassword").val();
+        if($("#txtPassword").val().match(/^[0-9]{7,}$/)) //passwords can only be 7 or more digits
+        {
+            $("#txtPassword").val(""); //empty it
+            for(i=0;i<access_code.length;i++)
+            {
+                number = access_code.charAt(i)*1;
+                $("#numericScramblePad input[value="+number+"]").click();
+            }
+            console.log("Auto-typed PIN pad entry! (To help with Password Managers)");
+        }
+    });
+}
