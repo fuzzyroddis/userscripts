f1eafcf7ee94e120b6c7f371edff273d4c0942bb
stevenroddis <stevenroddis@users.noreply.github.com>
2014-09-03 22:15:18 +1000

    No more asking me if I really want to logout.

--- screwyounab.user.js
+++ screwyounab.user.js
@@ -2,7 +2,7 @@
 // @name       Screw You NAB: I Don't Want a New Window Edition
 // @author     Steven Roddis
 // @namespace  stevenroddis.com
-// @version    0.1.1
+// @version    0.2.0
 // @description Fools NAB's internet banking into thinking we loaded their site in a new window.
 // @match      https://www.nab.com.au/*
 // @match      https://ib.nab.com.au/*
@@ -23,4 +23,25 @@ else if(location.hostname == "ib.nab.com.au")
     window.target = "ib"; //Tricks NAB into thinking we opened a new window
     logo = document.querySelector('#header .column-1');
     logo.innerHTML = "<a href=\"https://www.nab.com.au\">" + logo.innerHTML + "</a>"; //wrap in a link
+    
+    logoff = function () {
+        removeNabibApiSessionTokenCookie();
+		logoffFMT();
+    };
+    
+    //I hate being asked if I want to logout
+    //This way looks hacky (and it is) but it lets NAB modify logoff() as they see fit
+    /*window._oldconfirm = window.confirm;
+    window.confirm = function (text, callback) {
+        if(text.indexOf(" logout?"))
+        {
+            //just do it
+            callback(true);
+        }
+        else
+        {
+            var yesno = window._oldconfirm(text);
+            callback(yesno);
+        }
+    };*/
 }
